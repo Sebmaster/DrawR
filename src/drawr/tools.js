444bd7cbd0812404c8e40313180d206bc3308f6c
Sebastian Mayr <sebmaster16@gmail.com>
2013-02-14 08:50:35 +0100

    Refactors drawr into multiple files.
    The drawr.js file got pretty big and messy.

---
+++ src/drawr/tools.js
@@ -0,0 +1,274 @@
+DrawR.prototype.drawOutliner = function (touchPoints, start) {
+    this.activeLayer.ctx.beginPath();
+
+    var lastX = touchPoints[start].x;
+    var lastY = touchPoints[start].y;
+
+    this.activeLayer.ctx.moveTo(lastX, lastY);
+    for (var j = start + 1; j < touchPoints.length; ++j) {
+        this.activeLayer.ctx.lineTo(touchPoints[j].x, touchPoints[j].y);
+    }
+
+    this.activeLayer.ctx.strokeStyle = this.foregroundColor;
+    this.activeLayer.ctx.lineWidth = this.drawStyle.Outliner.minLineWidth * this.drawStyle.Outliner.lineWidth + (this.drawStyle.Outliner.lineWidth - this.drawStyle.Outliner.minLineWidth * this.drawStyle.Outliner.lineWidth) * touchPoints[start].force;
+    this.activeLayer.ctx.lineJoin = this.activeLayer.ctx.lineCap = 'round';
+    this.activeLayer.ctx.stroke();
+};
+DrawR.prototype.redrawDirtyOutliner = false;
+
+DrawR.prototype.determineDirtyOutliner = function (touchPoints, start) {
+	var minX = touchPoints[start].x;
+	var minY = touchPoints[start].y;
+	var maxX = minX;
+	var maxY = minY;
+	
+    for (var j = start + 1; j < touchPoints.length; ++j) {
+        minX = Math.min(touchPoints[j].x, minX);
+        minY = Math.min(touchPoints[j].y, minY);
+        maxX = Math.max(touchPoints[j].x, maxX);
+        maxY = Math.max(touchPoints[j].y, maxY);
+    }
+    
+    return {minX: minX - this.drawStyle.Outliner.lineWidth / 2,
+    	    minY: minY - this.drawStyle.Outliner.lineWidth / 2,
+    	    maxX: maxX + this.drawStyle.Outliner.lineWidth / 2,
+    	    maxY: maxY + this.drawStyle.Outliner.lineWidth / 2};
+};
+
+DrawR.prototype.drawBrush = function (touchPoints, start) {
+    this.activeLayer.ctx.beginPath();
+
+    var lastX = touchPoints[start].x;
+    var lastY = touchPoints[start].y;
+
+    this.activeLayer.ctx.moveTo(lastX, lastY);
+    for (var j = start; j < touchPoints.length; ++j) {
+        this.activeLayer.ctx.lineTo(touchPoints[j].x, touchPoints[j].y);
+    }
+
+    this.activeLayer.ctx.strokeStyle = this.foregroundColor;
+    this.activeLayer.ctx.lineWidth = this.drawStyle.Brush.minLineWidth * this.drawStyle.Brush.lineWidth + (this.drawStyle.Brush.lineWidth - this.drawStyle.Brush.minLineWidth * this.drawStyle.Brush.lineWidth) * touchPoints[start].force;
+    this.activeLayer.ctx.lineJoin = this.activeLayer.ctx.lineCap = 'round';
+    this.activeLayer.ctx.stroke();
+};
+
+DrawR.prototype.redrawDirtyBrush = false;
+
+DrawR.prototype.determineDirtyBrush = function (touchPoints, start) {
+	var lineWidth = this.drawStyle.Brush.minLineWidth * this.drawStyle.Brush.lineWidth + (this.drawStyle.Brush.lineWidth - this.drawStyle.Brush.minLineWidth * this.drawStyle.Brush.lineWidth) * touchPoints[0].force;
+	var minX = touchPoints[start].x;
+	var minY = touchPoints[start].y;
+	var maxX = minX;
+	var maxY = minY;
+	
+    for (var j = start + 1; j < touchPoints.length; ++j) {
+        minX = Math.min(touchPoints[j].x, minX);
+        minY = Math.min(touchPoints[j].y, minY);
+        maxX = Math.max(touchPoints[j].x, maxX);
+        maxY = Math.max(touchPoints[j].y, maxY);
+    }
+    
+    return {minX: minX - lineWidth / 2,
+    	    minY: minY - lineWidth / 2,
+    	    maxX: maxX + lineWidth / 2,
+    	    maxY: maxY + lineWidth / 2};
+};
+
+DrawR.prototype.drawEreaser = function (touchPoints, start) {
+    this.activeLayer.ctx.beginPath();
+
+    var lastX = touchPoints[start].x;
+    var lastY = touchPoints[start].y;
+
+    this.activeLayer.ctx.moveTo(lastX, lastY);
+    for (var j = start; j < touchPoints.length; ++j) {
+        this.activeLayer.ctx.lineTo(touchPoints[j].x, touchPoints[j].y);
+    }
+
+    var op = this.activeLayer.ctx.globalCompositeOperation;
+    this.activeLayer.ctx.globalCompositeOperation = 'copy';
+    this.activeLayer.ctx.lineWidth = this.drawStyle.Ereaser.lineWidth;
+    this.activeLayer.ctx.strokeStyle = 'rgba(0, 0, 0, 0)';
+    this.activeLayer.ctx.lineJoin = this.activeLayer.ctx.lineCap = 'round';
+    this.activeLayer.ctx.stroke();
+    this.activeLayer.ctx.globalCompositeOperation = op;
+};
+DrawR.prototype.redrawDirtyEreaser = false;
+
+DrawR.prototype.determineDirtyEreaser = function (touchPoints, start) {
+    var lineWidth = this.drawStyle.Ereaser.lineWidth;
+	var minX = touchPoints[start].x;
+	var minY = touchPoints[start].y;
+	var maxX = minX;
+	var maxY = minY;
+	
+    for (var j = start + 1; j < touchPoints.length; ++j) {
+        minX = Math.min(touchPoints[j].x, minX);
+        minY = Math.min(touchPoints[j].y, minY);
+        maxX = Math.max(touchPoints[j].x, maxX);
+        maxY = Math.max(touchPoints[j].y, maxY);
+    }
+    
+    return {minX: minX - lineWidth / 2,
+    	    minY: minY - lineWidth / 2,
+    	    maxX: maxX + lineWidth / 2,
+    	    maxY: maxY + lineWidth / 2};
+};
+
+DrawR.prototype.drawLine = function (touchPoints, start) {
+    this.activeLayer.ctx.beginPath();
+    this.activeLayer.ctx.moveTo(touchPoints[0].x, touchPoints[0].y);
+    this.activeLayer.ctx.lineTo(touchPoints[touchPoints.length - 1].x, touchPoints[touchPoints.length - 1].y);
+    this.activeLayer.ctx.lineWidth = this.drawStyle.Line.lineWidth;
+    this.activeLayer.ctx.lineCap = 'round';
+    this.activeLayer.ctx.strokeStyle = this.foregroundColor;
+    this.activeLayer.ctx.stroke();
+    
+    return {minX: touchPoints[0].x,
+    	    minY: touchPoints[0].y,
+    	    maxX: touchPoints[touchPoints.length - 1].x,
+    	    maxY: touchPoints[touchPoints.length - 1].y};
+};
+DrawR.prototype.redrawDirtyLine = true;
+
+DrawR.prototype.determineDirtyLine = function (touchPoints, start) {
+    var minX = touchPoints[0].x;
+    var minY = touchPoints[0].y;
+
+    var maxX = minX;
+    var maxY = minY;
+
+    for (var i = 1; i < touchPoints.length; ++i) {
+        minX = Math.min(touchPoints[i].x, minX);
+        minY = Math.min(touchPoints[i].y, minY);
+        maxX = Math.max(touchPoints[i].x, maxX);
+        maxY = Math.max(touchPoints[i].y, maxY);
+    }
+
+    return {
+        minX: minX - Math.ceil(this.drawStyle.Line.lineWidth / 2),
+        minY: minY - Math.ceil(this.drawStyle.Line.lineWidth / 2),
+        maxX: maxX + Math.ceil(this.drawStyle.Line.lineWidth / 2),
+        maxY: maxY + Math.ceil(this.drawStyle.Line.lineWidth / 2)
+    };
+};
+
+DrawR.prototype.drawBucket = function (touchPoints, start) {
+    var width = this.options.width;
+    var height = this.options.height;
+
+    var col = [0, 0, 0];
+    var drawStyle = this.foregroundColor;
+    col[0] = parseInt(drawStyle.substr(1, 2), 16);
+    col[1] = parseInt(drawStyle.substr(3, 2), 16);
+    col[2] = parseInt(drawStyle.substr(5, 2), 16);
+
+    var data = this.activeLayer.canvasData.data;
+    if (self.CanvasPixelArray && data instanceof self.CanvasPixelArray) { // IE hack because data is an array
+    	data = new Uint8Array(data);
+    }
+    var target = new Int32Array(data.buffer);
+    var finalCol;
+    
+    if (DrawR.littleEndian) {
+		finalCol = (
+			(255 << 24) |
+			(col[2] << 16) |
+			(col[1] << 8) |
+			 col[0]
+		);
+	} else {
+		finalCol = (
+			255 |
+			(col[2] << 8) |
+			(col[1] << 16) |
+			(col[0] << 24)
+		);
+	}
+
+    var touchPoint = touchPoints[touchPoints.length - 1];
+
+    var stack = [[touchPoint.x, touchPoint.y]];
+    var oldColor = target[touchPoint.x + touchPoint.y * width];
+
+    if (finalCol === oldColor) return;
+
+    var minX = touchPoint.x;
+    var minY = touchPoint.y;
+    var maxX = minX;
+    var maxY = minY;
+    var now;
+
+    while (now = stack.shift()) {
+        var arrPos = now[0] + now[1] * width;
+
+        var myColor = target[arrPos];
+
+        if (myColor !== oldColor) continue;
+
+        minX = Math.min(now[0], minX);
+        minY = Math.min(now[1], minY);
+        maxX = Math.max(now[0], maxX);
+        maxY = Math.max(now[1], maxY);
+
+        target[arrPos] = finalCol;
+
+        if (now[0] > 0) {
+            stack.push([now[0] - 1, now[1]]);
+        }
+        if (now[0] < this.options.width - 1) {
+            stack.push([now[0] + 1, now[1]]);
+        }
+        if (now[1] > 0) {
+            stack.push([now[0], now[1] - 1]);
+        }
+        if (now[1] < this.options.height - 1) {
+            stack.push([now[0], now[1] + 1]);
+        }
+    }
+
+	if (self.CanvasPixelArray && this.activeLayer.canvasData.data instanceof self.CanvasPixelArray) {
+		var backData = this.activeLayer.canvasData.data;
+		
+		for (var i=minY; i <= maxY + 1; ++i) {
+			for (var j=minX; j <= maxX + 1; ++j) {
+				var pos = (j + i * width) * 4;
+				backData[pos] = data[pos];
+				backData[pos + 1] = data[pos + 1];
+				backData[pos + 2] = data[pos + 2];
+				backData[pos + 3] = data[pos + 3];
+			}
+		}
+	}
+    this.activeLayer.ctx.putImageData(this.activeLayer.canvasData, 0, 0, minX, minY, maxX - minX + 1, maxY - minY + 1);
+    
+    return {minX: minX,
+    	    minY: minY,
+    	    maxX: maxX,
+    	    maxY: maxY};
+};
+DrawR.prototype.redrawDirtyBucket = false;
+
+DrawR.prototype.determineDirtyBucket = function (touchPoints, start) {
+    return null;
+};
+
+DrawR.prototype.hsvFilter = function(h, s, v) {
+    var imageData = this.activeLayer.canvasData.data;
+    
+    for (var i=0; i < this.options.width; ++i) {
+        for (var j=0; j < this.options.height; ++j) {
+            var pos = i * this.options.height + j;
+            var hsv = DrawR.rgbToHsv(imageData[pos], imageData[pos + 1], imageData[pos + 2]);
+            hsv.h += h;
+            hsv.s += s;
+            hsv.v += v;
+            var rgb = DrawR.hsvToRgb(hsv.h, hsv.s, hsv.v);
+            imageData[pos] = rgb.r;
+            imageData[pos + 1] = rgb.g;
+            imageData[pos + 2] = rgb.b;
+        }
+    }
+    
+    this.activeLayer.ctx.putImageData(this.activeLayer.canvasData, 0, 0);
+};
